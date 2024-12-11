4d6fa769a7a6899b746b715705ed749f160e1975
Sjoerd Dost <deosjr@gmail.com>
2024-12-11 09:18:36 +0100

    day11p2 in go

--- 11/day11.go
+++ 11/day11.go
@@ -14,8 +14,8 @@ func main() {
     for i, s := range split {
         input[i] = lib.MustParseInt(s)
     }
-    ans1 := len(p1(input))
-    lib.WritePart1("%v", ans1)
+    lib.WritePart1("%v", p1(input))
+    lib.WritePart2("%v", p2(input))
 }
 
 func blink(stone int64) []int64 {
@@ -32,14 +32,42 @@ func blink(stone int64) []int64 {
    return []int64{stone * 2024}
 }
 
-func p1(list []int64) []int64 {
-    newlist := []int64{}
-    for i:=0; i<25; i++ {
-        for _, s := range list {
-            newlist = append(newlist, blink(s)...)
-        }
-        list = newlist
-        newlist = []int64{}
+type in struct {
+    stone int64
+    n int
+}
+
+var mem = map[in]int64{}
+
+func blinkRec(stone int64, n int) int64 {
+    if v, ok := mem[in{stone, n}]; ok {
+        return v
     }
-    return list
+    rec := blink(stone)
+    if n == 1 {
+        mem[in{stone, n}] = int64(len(rec))
+        return int64(len(rec))
+    }
+    var sum int64
+    for _, s := range rec {
+        sum += blinkRec(s, n-1)
+    }
+    mem[in{stone, n}] = sum
+    return sum
+}
+
+func p1(list []int64) int64 {
+    var sum int64
+    for _, s := range list {
+        sum += blinkRec(s, 25)
+    }
+    return sum
+}
+
+func p2(list []int64) int64 {
+    var sum int64
+    for _, s := range list {
+        sum += blinkRec(s, 75)
+    }
+    return sum
 }
