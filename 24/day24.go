fb59a1f25a8824dadc79b8fe412b2d3e257c9bb0
Sjoerd Dost <deosjr@gmail.com>
2024-12-24 10:18:40 +0100

    day24p2

--- 24/day24.go
+++ 24/day24.go
@@ -46,6 +46,9 @@ func main() {
         }
         gates = append(gates, g)
     }
+
+    //generateMermaid(fringe, gates)
+
     for len(fringe) > 0 {
         var g gate
         for k := range fringe {
@@ -71,4 +74,49 @@ func main() {
         ans |= b
     }
     lib.WritePart1("%d", ans)
+
+    // part2 done by hand looking at mermaid output
+    // swaps: ggn,grm,jcb,ndw,twr,z10,z32,z39
+    lib.WritePart2("ggn,grm,jcb,ndw,twr,z10,z32,z39")
+}
+
+// clearly defining an adder, with each input x/y going to 
+// a halfadder: carry = AND(x,y); sum = XOR(x,y)
+// two halfadders make a full adder, with a carry bit as third input
+// c1, s1 = halfadd(x, y); c2, s2 = halfadd(s1, carry); then
+// add(x,y,c): carry = OR(c1, c2); sum = s2
+// NOTES: all outputs (except last) should be coming from XOR
+// ISSUES: z32 and z39 coming from an AND
+// z10 is coming from an OR
+// XOR[grm] needs to go to out but doesnt (its out z32)
+// XOR[twr] needs to go to out but doesnt (its out z39)
+// XOR[ggn] should output z10 but doesnt
+// AND[ndw] goes to two but should only go to one
+// XOR[jcb] is sus
+func generateMermaid(inputs map[gate]struct{}, rest []gate) {
+    // gvm OR smt -> z10    SWAP    mbv XOR hks -> ggn
+    // rmn AND whq -> z32   SWAP    rmn XOR whq -> grm
+    // x39 AND y39 -> z39   SWAP    pqv XOR bnv -> twr
+    fmt.Println("flowchart TD")
+    for g := range inputs {
+        if g.x == "x39" && g.op == "AND" {
+            g.z = "twr"
+        }
+        fmt.Printf("%s --> |%s| %s[%s]\n", g.x, g.x, g.z, g.op)
+        fmt.Printf("%s --> |%s| %s[%s]\n", g.y, g.y, g.z, g.op)
+    }
+    for _, g := range rest {
+        switch {
+        case g.x == "gvm" && g.op == "OR": g.z = "ggn"
+        case g.x == "mbv" && g.op == "XOR": g.z = "z10"
+        case g.x == "rmn" && g.op == "AND": g.z = "grm"
+        case g.x == "rmn" && g.op == "XOR": g.z = "z32"
+        case g.x == "pqv" && g.op == "XOR": g.z = "z39"
+        }
+        fmt.Printf("%s --> |%s| %s[%s]\n", g.x, g.x, g.z, g.op)
+        fmt.Printf("%s --> |%s| %s[%s]\n", g.y, g.y, g.z, g.op)
+    }
+    for i:=0; i<46; i++ {
+        fmt.Printf("z%02d --> |z%02d| output\n", i, i)
+    }
 }
